04edcc78ecbea581d75850143aa16d4ded4c0d48
berkax <128898562+umwtlister@users.noreply.github.com>
2023-05-05 00:40:44 +0300

    Add files via upload

---
+++ com/berkax/github/GlowSystems$1.java
@@ -0,0 +1,16 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+package com.berkax.github;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+class GlowSystems$1 implements ActionListener
+{
+    @Override
+    public void actionPerformed(final ActionEvent e) {
+        GlowSystems.webhook();
+    }
+}
